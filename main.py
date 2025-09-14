f0a578979a791ac4085b1ee3524328ed94b87681
MD AL RAKEB RASEL BOSHUNIA <official.alrakib@gmail.com>
2025-09-14 18:24:38 +0600

    initial project

---
+++ main.py
@@ -0,0 +1,45 @@
+#name=input("Enter your name:")
+#print("Hello "+ name +" !")
+
+#simple calculator project
+"""
+num1=input("Enter the first number: ")
+num2=input("Enter the second number: ")
+num3=input("Enter the third number: ")
+num4=input("Enter the forth number: ")
+
+num1=int(num1)
+num2=int(num2)
+num3=int(num3)
+num4=int(num4)
+
+total=num1+num2-num3*num4
+
+print("The sum is: ",total)
+"""
+
+#Celsius to fahrenheit project
+"""
+celsius=input("Enter the celsius: ")
+celsius=float(celsius)
+
+fahrenheit=9/5*celsius+32
+
+print("The fahrenheit is: ",fahrenheit)
+"""
+
+#principal interest project
+"""
+principal=input("Enter the principal amount: ")
+rate=input("Enter the rate of interest: ")
+time=input("Enter the rate in years: ")
+
+principal=float(principal)
+rate=float(rate)
+time=float(time)
+
+interest=(principal*rate*time)/100
+
+print("The interest is: ",interest)
+"""
+
